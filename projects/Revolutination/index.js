1f0b0e0b3231e7ade84b8e9152b6d26632de1804
KOSASIH <kosasihg88@gmail.com>
2023-08-05 21:25:57 +0700

    Create index.js

---
+++ projects/Revolutination/index.js
@@ -0,0 +1,13 @@
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+
+// If used on the FRONTEND pass your 'clientId'
+const sdk = new ThirdwebSDK("mumbai", {
+  clientId: "YOUR_CLIENT_ID",
+});
+// --- OR ---
+// If used on the BACKEND pass your 'secretKey'
+const sdk = new ThirdwebSDK("mumbai", {
+  secretKey: "YOUR_SECRET_KEY",
+});
+
+const contract = await sdk.getContract("0x23D0328f85feFb3C5dAA3e042b0692206E761421");
