0fb896193dc48e1c9947d45ed2500718463d9590
KOSASIH <kosasihg88@gmail.com>
2023-08-09 13:08:42 +0700

    Create index.js

---
+++ projects/Spacemart/index.js
@@ -0,0 +1,13 @@
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+
+// If used on the FRONTEND pass your 'clientId'
+const sdk = new ThirdwebSDK("mumbai", {
+  clientId: "YOUR_CLIENT_ID",
+});
+// --- OR ---
+// If used on the BACKEND pass your 'secretKey'
+const sdk = new ThirdwebSDK("mumbai", {
+  secretKey: "YOUR_SECRET_KEY",
+});
+
+const contract = await sdk.getContract("0x64bD77Ada57F2d5cFa7b0ca6FD2e9A30ffFadC73");
